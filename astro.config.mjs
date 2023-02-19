39d40067bd98c98e75dd616ae0a5a19f5b0bf8c7
tris <nexus4fortune@gmail.com>
2023-02-19 09:38:40 +0900

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://tris5572.github.io/",
+  base: "/astro-test",
+  integrations: [mdx(), sitemap()],
 });
